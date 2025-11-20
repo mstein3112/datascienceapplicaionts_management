1af4d07046e5ae030c924d2c7d9f081db97dc4f0
mstein3112 <sm6691@mci4me.at>
2025-11-20 09:15:43 +0100

    functions

--- text_magic/functions.py
+++ text_magic/functions.py
@@ -1,5 +1,8 @@
 from string import punctuation
+
+
 PUNCTUATIONS = punctuation + "“”"
+
 
 def clean_to_lower(text:str) -> str:
     return text.lower()
@@ -11,3 +14,13 @@ def clean_punctuation(text:str) -> str:
             output_string += character
     return output_string
 
+def clean_strip(text:str) -> str:
+    for _ in range(10):
+        text = text.replace("  ", " ")
+    return text.strip()
+
+def pipeline(text:str) -> str:
+    text = clean_to_lower(text)
+    text = clean_punctuation(text)
+    text = clean_strip(text)
+    return text
